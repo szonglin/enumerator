83b1ab546e05036572d7b5fd553856167ea6bd09
zonglin <z5477757@ad.unsw.edu.au>
2025-04-08 16:45:15 +1000

    comments for util methods

--- src/en/util.ts
+++ src/en/util.ts
@@ -1,33 +1,7 @@
 export type enumerationType = "permutation" | "combination";
 
 export class Util {
-  // converts a string input to an array of numbers
-  static inputToArray = (input: string): number[] => {
-    input = input.trim();
-    let res;
-    if (input.includes(",")) {
-      res = input.replace(/\s+/g, "").split(",");
-    } else if (input.includes(" ")) {
-      res = input.split(" ");
-    } else {
-      res = input.split("");
-    }
-    return res.map((e) => {
-      const _e = Number(e);
-      if (!isNaN(_e)) return _e;
-      else if (e.length === 1) return e.charCodeAt(0);
-      else return NaN;
-    });
-  };
-
-  // checks that a string does not contain any characters that are not ' ', ',' , a number, or a character ascii
-  static validateString = (input: string) => {
-    if (/[^a-zA-Z0-9\s,]/.test(input))
-      throw new Error(
-        "Invalid input: please use only spaces, commas, numbers, or alphabetical characters.",
-      );
-  };
-
+  /* Math functions */
   // evaluates the factorial of a number
   static factorial(n: number): number {
     return n === 0 ? 1 : n * this.factorial(n - 1);
@@ -50,23 +24,7 @@ export class Util {
     return Math.round(a / b);
   }
 
-  // next permutation algorithm
-  // adapted from cppreference
-  static nextPermutation(arr: number[]): boolean {
-    let left = arr.length - 2;
-    while (left >= 0 && arr[left] >= arr[left + 1]) left--;
-    if (left === -1) return false;
-
-    let right = arr.length - 1;
-    while (arr[right] <= arr[left]) right--;
-
-    [arr[left], arr[right]] = [arr[right], arr[left]];
-    for (let l = left + 1, r = arr.length - 1; l < r; l++, r--) {
-      [arr[l], arr[r]] = [arr[r], arr[l]];
-    }
-    return true;
-  }
-
+  // all values of array are distinct
   static isDistinct(arr: number[]): boolean {
     return new Set(arr).size === arr.length;
   }
@@ -86,6 +44,26 @@ export class Util {
     return res;
   }
 
+  /* Array conversion methods */
+  // converts a string input to an array of numbers
+  static inputToArray = (input: string): number[] => {
+    input = input.trim();
+    let res;
+    if (input.includes(",")) {
+      res = input.replace(/\s+/g, "").split(",");
+    } else if (input.includes(" ")) {
+      res = input.split(" ");
+    } else {
+      res = input.split("");
+    }
+    return res.map((e) => {
+      const _e = Number(e);
+      if (!isNaN(_e)) return _e;
+      else if (e.length === 1) return e.charCodeAt(0);
+      else return NaN;
+    });
+  };
+
   // converts a number to an ascii character if it is alphabetical, number otherwise
   static permElementToString(e: number): string {
     return (e >= 65 && e <= 90) || (e >= 97 && e <= 122)
@@ -98,50 +76,33 @@ export class Util {
     return Array.from(new Set(arr).keys()).sort((a, b) => a - b);
   }
 
-  // random int
-  static randInt(upperBound: number): number {
-    return Math.floor(Math.random() * upperBound);
-  }
+  // standard next permutation algorithm
+  // adapted from cppreference implementation
+  static nextPermutation(arr: number[]): boolean {
+    let left = arr.length - 2;
+    while (left >= 0 && arr[left] >= arr[left + 1]) left--;
+    if (left === -1) return false;
 
-  // random array element
-  static randElement(arr: number[]): number {
-    return arr[this.randInt(arr.length)];
-  }
+    let right = arr.length - 1;
+    while (arr[right] <= arr[left]) right--;
 
-  // random permutation
-  static randPerm(arr: number[], length: number): number[] {
-    if (length > arr.length)
-      throw new Error("Cannot permute array to longer length");
-    return this.randPermFull(arr).splice(0, length);
-  }
-
-  // fisher-yates shuffle: https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
-  static randPermFull(arr: number[]): number[] {
-    const permed = [...arr];
-    for (let i = permed.length - 1; i > 0; i--) {
-      const j = this.randInt(i + 1);
-      [permed[j], permed[i]] = [permed[i], permed[j]];
+    [arr[left], arr[right]] = [arr[right], arr[left]];
+    for (let l = left + 1, r = arr.length - 1; l < r; l++, r--) {
+      [arr[l], arr[r]] = [arr[r], arr[l]];
     }
-    return permed;
+    return true;
   }
 
-  // random permutation with repeats, arr must not have repeats
-  static randPermRp(arr: number[], length: number): number[] {
-    return Array.from({ length }, () => this.randElement(arr));
-  }
+  /* Array comparison methods */
+  // checks that a string does not contain any characters that are not ' ', ',' , a number, or a character ascii
+  static validateString = (input: string) => {
+    if (/[^a-zA-Z0-9\s,]/.test(input))
+      throw new Error(
+        "Invalid input: please use only spaces, commas, numbers, or alphabetical characters.",
+      );
+  };
 
-  // random choice - output is not guaranteed to be sorted
-  static randChoice(arr: number[], length: number): number[] {
-    // honestly not sure if its the same distribution to just do fisheryates and then sort but this *feels* better
-    const bitmask = this.randPermFull(this.mask(arr.length, length));
-    const res = [];
-    // faster than reduce
-    for (let i = 0; i < arr.length; i++) {
-      if (bitmask[i]) res.push(arr[i]);
-    }
-    return res;
-  }
-
+  // arr submultiset of of
   static subMultiset(arr: number[], of: number[]): boolean {
     const ms = new Map<number, number>();
     for (const e of of) {
@@ -156,6 +117,7 @@ export class Util {
     return true;
   }
 
+  // arr prefix of of
   static isPrefix(arr: number[], of: number[]) {
     if (arr.length > of.length) return false;
     for (let i = 0; i < arr.length; i++) {
@@ -164,6 +126,7 @@ export class Util {
     return true;
   }
 
+  // arr suffix of of
   static isSuffix(arr: number[], of: number[]) {
     if (arr.length > of.length) return false;
     for (let i = 0; i < arr.length; i++) {
@@ -172,11 +135,62 @@ export class Util {
     return true;
   }
 
+  /* Random generators */
+  // random int
+  static randInt(upperBound: number): number {
+    return Math.floor(Math.random() * upperBound);
+  }
+
+  // random array element, assumes array is distinct
+  static randElement(arr: number[]): number {
+    return arr[this.randInt(arr.length)];
+  }
+
+  // random permutation
+  // performs a full random permutation on the array and splices out the
+  // prefix of the correct length
+  static randPerm(arr: number[], length: number): number[] {
+    if (length > arr.length)
+      throw new Error("Cannot permute array to longer length");
+    return this.randPermFull(arr).splice(0, length);
+  }
+
+  // generates a random permutation of an array using a fisher-yates shuffle
+  // adapted from https://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle
+  static randPermFull(arr: number[]): number[] {
+    const permed = [...arr];
+    for (let i = permed.length - 1; i > 0; i--) {
+      const j = this.randInt(i + 1);
+      [permed[j], permed[i]] = [permed[i], permed[j]];
+    }
+    return permed;
+  }
+
+  // random permutation with repeats, arr must not have repeats
+  // repeatedly takes a random index of the array
+  static randPermRp(arr: number[], length: number): number[] {
+    return Array.from({ length }, () => this.randElement(arr));
+  }
+
+  // random choice - output is not guaranteed to be sorted
+  // we take a random perm of a bitmask and mask out the corresponding
+  // values in the choice
+  static randChoice(arr: number[], length: number): number[] {
+    const bitmask = this.randPermFull(this.mask(arr.length, length));
+    const res = [];
+    // faster than reduce
+    for (let i = 0; i < arr.length; i++) {
+      if (bitmask[i]) res.push(arr[i]);
+    }
+    return res;
+  }
+
   // random choice with repeats, arr must not have repeats
+  // this doesn't work because its not uniform after sorting
+  //  return this.randPermRp(arr, length).sort((a, b) => a - b);
+  // instead we use a bitmask to generate a random stars and bars permutation
+  // and take the corresponding combination
   static randChoiceRp(arr: number[], length: number): number[] {
-    // this doesn't work because its not uniform after sorting
-    // return this.randPermRp(arr, length).sort((a, b) => a - b);
-    // instead we use s&b with |s| = length and |b| = arr.length - 1
     let g = this.mask(arr.length + length - 1, length);
     g = this.randPermFull(g);
     const res: number[] = [];
